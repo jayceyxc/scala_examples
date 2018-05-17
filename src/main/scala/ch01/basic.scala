c219000d7578a5528a73e6ccc9d78a2467434f33
Yuxuecheng <jayce123@163.com>
2018-05-17 10:08:52 +0800

    Initial commit the scala project

---
+++ src/main/scala/ch01/basic.scala
@@ -0,0 +1,14 @@
+package ch01
+
+object basic {
+  def main(args: Array[String]): Unit = {
+    val xmax, ymax = 100
+    var greeting, message: String = "hello world"
+
+    println(xmax)
+    println(ymax)
+    println(greeting)
+    println(message)
+  }
+
+}
